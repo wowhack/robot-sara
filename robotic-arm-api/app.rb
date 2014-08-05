f75a7e3ae098f240c14cb497aa76c03ee82db3e5
Aaron Randall <aaronjrandall@gmail.com>
2014-08-05 12:13:36 +0000

    Ignore swapfiles. Start of Robotic arm API.

--- robotic-arm-api/app.rb
+++ robotic-arm-api/app.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
-require './lib/robotic_arm.rb'
+require_relative 'lib/robotic_arm.rb'
 
 set :port, 1025
+set :bind, '0.0.0.0'
+
+def robotic_arm
+  @@robotic_arm ||= RoboticArm.new
+end
+
+def with_retry(&block)
+  tries ||= 3
+
+  begin
+    block.call
+  rescue Exception => e
+    retry unless (tries -= 1).zero?
+  end
+end
 
 get '/' do
-  "PONG"
+  with_retry do
+    robotic_arm.perform_action(RoboticArm::BASE_RIGHT, 1)
+  end
 end
